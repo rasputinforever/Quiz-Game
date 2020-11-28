1eee287ea5fca6a8def02ed98ddd652a60ef0f39
Erik Portillo <Erik.Portillo@dcusa.tentamus.com>
2020-11-28 14:28:39 -0800

    created and tested js array of quiz objects

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,31 @@
-console.log('hello');
+//quiz questions and answers
+var quizQuestions = [
+    {
+        question: "Please click the second answer.",
+        choices: ["one", "two", "three", "four"],
+        answer: 2
+    },
+    {
+        question: "Please click the fourth answer.",
+        choices: ["one", "two", "three", "four"],
+        answer: 4
+    },
+    {
+        question: "Please click the fourth answer.",
+        choices: ["one", "two", "three", "four"],
+        answer: 4
+    },
+    {
+        question: "Please click the second answer.",
+        choices: ["one", "two", "three", "four"],
+        answer: 2
+    },
+    {
+        question: "Please click the first answer.",
+        choices: ["one", "two", "three", "four"],
+        answer: 1
+    }
+];
+
+
+
